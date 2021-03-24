95fa6a46b66fb63442f7800217f11e505947de78
TomasRacil <tomas.racil@unob.cz>
2021-03-24 13:11:48 +0100

    hodnost added

---
+++ backend/app/main/model/hodnost.py
@@ -0,0 +1,19 @@
+from .. import db
+
+class Hodnost(db.Model):
+    """
+    Model pro ukládání jenotlivých hodností a hodnostních zborů.
+    """
+    __tablename__="hodnost"
+
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    hodnost = db.Column(db.String(20), unique=True, nullable=False)
+    hodnostni_sbor = db.Column(db.String(20), nullable=False)
+
+    def __init__ (self, hodnost, hodnostni_sbor):
+        self.hodnost=hodnost
+        self.hodnostni_sbor=hodnostni_sbor
+
+    def __repr__(self):
+        return f"hodnost: {self.hodnost} hodnostni_sbor: {self.hodnostni_sbor}"
