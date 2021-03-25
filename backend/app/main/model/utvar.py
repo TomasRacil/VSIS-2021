5e05ab15c3c929adc6ef50fc7196b59a4a6496ab
chlumovav <wendychlumova@email.cz>
2021-03-25 09:32:40 +0100

    main

---
+++ backend/app/main/model/utvar.py
@@ -0,0 +1,23 @@
+from .. import db
+"""
+    Model pro ukládání názvu útvaru, jeho lokace a čísla VÚ/VZ
+"""
+class utvar(db.Model):
+    __tablename__="utvar"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nazev_utvaru = db.Column(db.String(50), nullable=False)
+    lokace = db.Column(db.String(30), nullable=False)
+    cislo_vu = db.Column(db.Integer(10), unique=True, nullable=False)
+
+    #osoba_id = db.Column(db.Integer, db.ForeignKey('id_osoba'), nullable=False)
+    #osoba = db.relationship('Osoba', backref=db.backref('posts', lazy=True))
+
+    
+    def __init__ (self, nazev_utvaru, lokace, cislo_vu):
+        self.nazev_utvaru=nazev_utvaru
+        self.lokace=lokace
+        self.cislo_vu=cislo_vu
+
+    def __repr__(self):
+              return f"nazev_utvaru: {self.nazev_utvaru} lokace: {self.lokace} cislo_vu: {self.cislo_vu}"
