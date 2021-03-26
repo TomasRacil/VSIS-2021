993f6dbdc1d30c8b0639782bb59084b2ef092a51
TomasRacil <tomas.racil@unob.cz>
2021-03-26 10:30:12 +0100

    osoba-kurz changed

--- backend/app/main/model/kurz.py
+++ backend/app/main/model/kurz.py
@@ -1,5 +1,11 @@
 from .. import db
 
+
+osoba_kurz = db.Table('osoba_kurz',
+    db.Column('osoba_id', db.Integer, db.ForeignKey('osoba.id'), primary_key=True),
+    db.Column('kurz_id', db.Integer, db.ForeignKey('kurz.id'), primary_key=True)
+)
+
 class Kurz(db.Model):
     """
     Model pro ukládání jenotlivých kurzů.
@@ -14,6 +20,8 @@ class Kurz(db.Model):
     voj_oznaceni = db.Column(db.String(20), nullable=False)
     zacatek_kurzu = db.Column(db.DateTime, nullable=False)
     konec_kurzu = db.Column(db.DateTime, nullable=False)
+
+    osoby = db.relationship('Osoba', secondary=osoba_kurz, lazy='dynamic',backref=db.backref('kurzy', lazy='dynamic'))
 
     # def __init__ (self, nazev, misto, vedouci, voj_oznaceni, zacatek_kurzu, konec_kurzu):
     #     self.nazev=nazev
--- backend/manage.py
+++ backend/manage.py
@@ -6,7 +6,7 @@ from flask_script import Manager
 
 from app import blueprint
 from app.main import create_app, db
-from app.main.model import user, role, blacklist, hodnost, osoba, osoba_kurz, kurz, utvar
+from app.main.model import user, role, blacklist, hodnost, osoba, kurz, utvar
 
 app = create_app(os.getenv('BOILERPLATE_ENV') or 'dev')
 app.register_blueprint(blueprint)
